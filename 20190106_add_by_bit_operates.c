d7f9fb4139d7e26d9a7672c3fd601cd6ce62b26f
shidenggui <903618848@qq.com>
2019-01-06 17:02:01 +0800

    :star: 20190106_add_by_bit_operates.c

---
+++ 20190106_add_by_bit_operates.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+
+int add(int n1, int n2) {
+    int sum;
+    // 因为 sum + carry 很可能还是会溢出，所以需要循环到 carry == 0 为止
+    while (n2 != 0) {
+        // sum 保存了 n1 + n2 但是不进位的结果
+        sum = n1 ^ n2;
+        // n2 保存了 n1 + n2 的所有进位
+        n2 = (n1 & n2) << 1;
+        n1 = sum;
+    }
+    return n1;
+}
+
+void test_add() {
+    assert(add(1, 3) == 4);
+    assert(add(17, 5) == 22);
+    assert(add(17, 15) == 32);
+}
+
+int main() {
+    test_add();
+}
