3ab95a5d9c36534d7c82bdce7dec2014ec9ba5f8
shidenggui <903618848@qq.com>
2019-01-07 13:38:07 +0800

    :star: 20190107_add_by_bit_operates.c

--- 20190106_add_by_bit_operates.c
+++ 20190106_add_by_bit_operates.c
@@ -17,6 +17,9 @@ void test_add() {
     assert(add(1, 3) == 4);
     assert(add(17, 5) == 22);
     assert(add(17, 15) == 32);
+    assert(add(-1, -3) == -4);
+    assert(add(-10, 3) == -7);
+    assert(add(10, -14) == -4);
 }
 
 int main() {
