538df2e8c70741de2ca51aa2d4be376fffcb9f5d
shidenggui <903618848@qq.com>
2019-01-26 14:58:15 +0800

    :star: 20190126_leetcode_163_missing_range.py

--- 20190126_leetcode_163_missing_range.py
+++ 20190126_leetcode_163_missing_range.py
@@ -24,3 +24,19 @@ def test():
     assert(str(missing_range([], 0, 99)) == "['0->99']")
     assert(str(missing_range([3], 0, 99)) == "['0->2', '4->99']")
     assert(str(missing_range(list(range(0, 100)), 0, 99)) == "[]")
+
+# C++ 
+public List<String> findMissingRanges(int[] vals, int start, int end) {
+    List<String> ranges = new ArrayList<>();
+    int prev = start - 1;
+    for (int i = 0; i <= vals.length; i++) {
+        int curr = (i == vals.length) ? end + 1 : vals[i];
+        if (curr - prev >= 2) { ranges.add(getRange(prev + 1, curr - 1)); }
+        prev = curr;
+    }
+    return ranges;
+}
+
+private String getRange(int from, int to) {
+    return (from == to) ? String.valueOf(from) : from + "->" + to; 
+}
